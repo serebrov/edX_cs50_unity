71ece36ae36a10154c8febfb7d1525d4bb485b7e
Boris Serebrov <serebrov@gmail.com>
2023-11-26 11:04:53 -0500

    Portal submission: jumping puzzle.

---
+++ portal_10/Assets/Scripts/LevelUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelUp : MonoBehaviour
+{
+    public GameObject levelUpText;
+    public Color color;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Text uiText = levelUpText.GetComponent<Text>();
+        uiText.color = color;
+        uiText.text = text;
+
+    }
+}
---
+++ portal_10/Assets/Scripts/TeleportToStart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class TeleportToStart : MonoBehaviour
+{
+    public GameObject teleportTo;
+    public GameObject levelUpText;
+    public int offset = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log ("Trigger entered!");
+
+        Text uiText = levelUpText.GetComponent<Text>();
+        uiText.color = new Color(0, 0, 0, 0);
+
+        // set the player's position of the start block
+        other.transform.SetPositionAndRotation(
+            teleportTo.transform.position,
+            Quaternion.identity);
+    }
+}
